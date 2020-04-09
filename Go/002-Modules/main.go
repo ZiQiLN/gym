9a7416ace7eb7b3dd7958d23235773c0e64dd4dd
Jakub Olan <jakub.olan001@gmail.com>
2020-04-10 01:25:25 +0200

    Variables in Go
    A little introduction to using variables in Golang.

--- Go/002-Modules/main.go
+++ Go/002-Modules/main.go
@@ -1,7 +1,11 @@
+// Initializing a main package.
 package main
 
+// Importing a 'fmt' package which allows to IO Events.
 import "fmt"
 
+// Declaring a main function.
 func main() {
-  fmt.Print("Hello from Module")
+	// Using a print function to std::out a string with "Hello World"
+	fmt.Print("Hello World")
 }
---
+++ Go/003-Variables/main.go
@@ -0,0 +1,31 @@
+// Initializing a new package called main
+package main
+
+// Importing a fmt package
+import (
+	"fmt"
+)
+
+// Declaring a function called Main
+func main() {
+
+	// Initializing a new variable named "number" with type of integrer.
+	var number int
+
+	// Aliging a value equal to 42 to variable named "number"
+	number = 42
+
+	// Printing out a value of variable "number" with a line break statement.
+	fmt.Print(number, "\n")
+
+	// Initializing a floating-point variable, in Go there are two types of floating-point variables a float32s and float64s which represents a bits of specified variable - 32bit and 64bit. We can also directly add a value to specified variable by using a "=" and then value.
+	var float float32 = 13.37
+	fmt.Print(float, "\n")
+
+	// Golang have also support for contants, which are a read-only variables.
+	const helloWorld string = "Hello World as Constant"
+
+	// Golang have also a quick way for defining a new variables, which is more comfortable to use when we need define something on the fly. This method automatically resolving a type of variable.
+	sampleVariable := "Hello World"
+	fmt.Print(sampleVariable, "\n")
+}
